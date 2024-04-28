a66dc824a0192d7590c3235df7896f8dc8c24d2a
Alisson Oliveira <alisson.silva.oliveira01@aluno.ifce.edu.br>
2024-04-27 21:36:34 -0300

    adicionado corpo do sistema

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da agência: ");
+        int agencia = scanner.nextInt();
+        System.out.println("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+        System.out.println("Digite o nome do nome do cliente: ");
+        String nomeCliente = scanner.next();
+        System.out.println("Digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("");
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " e sua conta é " + conta + " e seu saldo é de " + saldo + " reais e já está disponível para saque.");
+        System.out.println("");
+        
+        scanner.close();
     }
 }

808a9e1d3ecca8caf3b1b8ac9d6e9e4aba2466aa
Alisson Oliveira <alisson.silva.oliveira01@aluno.ifce.edu.br>
2024-05-22 10:58:35 -0300

    commit inicial

---
+++ collections-java-api/src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,14 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    
+}
